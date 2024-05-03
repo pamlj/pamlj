f6ad4303d20032cbf0863a1a3298ba9fdec81540
mcfanda <mcfanda@gmail.com>
2024-05-03 12:56:03 +0200

    fix some glm stuff

--- R/effectsize.R
+++ R/effectsize.R
@@ -1,18 +1,23 @@
 
-effectsize <- function(obj, ...) UseMethod(".effectsize")
+effectsize_init <- function(obj, ...) UseMethod(".effectsize_init")
+effectsize_run <- function(obj, ...) UseMethod(".effectsize_run")
 
-.effectsize.default <- function(obj) return()
+.effectsize_init.default <- function(obj) return()
+.effectsize_run.default <- function(obj) return()
 
-.effectsize.beta <- function(obj) {
 
-  if (nrow(obj$analysis$data)==0)
+.effectsize_init.beta <- function(obj) {
+
     return(list(
                list(index=letter_r2),
                list(index="Effect tollerance"),
                list(index=paste("Effect",letter_peta2)),
                list(index="Model df")
                  ))
-  
+}
+
+.effectsize_run.beta <- function(obj) {
+
    tab <- list()
    ladd(tab)<-list(value=obj$data$r2)
    tol<-1-obj$data$ri2
@@ -24,7 +29,8 @@ effectsize <- function(obj, ...) UseMethod(".effectsize")
   
 }
 
-.effectsize.eta <- function(obj) {
+
+.effectsize_init.eta <- function(obj) {
 
    tab <- list()
    ladd(tab)<-list(index=letter_r2,value=obj$data$r2)
@@ -32,5 +38,11 @@ effectsize <- function(obj, ...) UseMethod(".effectsize")
    if (length(peta)==0) peta<-"."
    ladd(tab)<-list(index=paste("Effect",letter_peta2),value=peta)
    return(tab)
+  
+}
+
+.effectsize_run.eta <- function(obj) {
+
+   .effectsize_init.eta(obj)
   
 }
