6f37ff39ca37db09cc9db90d7be9c61baaa82c3a
mcfanda <mcfanda@gmail.com>
2024-09-11 19:53:29 +0200

    fix sobel test

--- R/Dispatch.R
+++ R/Dispatch.R
@@ -133,61 +133,70 @@ Dispatch <- R6::R6Class(
                        errors_topics=function() {return(names(private$.errors))}
                       
             ),
-            private = list(
-                      .warnings=list(),
-                      .errors=list(),
-                      .process_html= function(content,obj) {
-                        
-                        style=""
-                        title=""
-
-                        if (is.something(obj$head)) {
-                                    switch (obj$head,
-                                          "info"     =  {
-                                                         head<-"<div class='icon info' style=' width:50px; height:50px'></div>"
-                                                         style<-" border-color: #3e6da9"
-                                                         },
-                                          "warning"   =  {
-                                                          head <- "<div class='icon warning-2' style=' min-width:20px; min-height:50px'></div>"
-                                                          style="border-left-color: red"
-                                                          title<-"<h2 style='color:red'> Warning</h2>"
-
-                                                         },
-                                          "error"   =    {
-                                                         head <- "<div class='icon error' style=' min-width:20px; min-height:20px'></div>"
-                                                         style="border-color: red"
-                                                         title<-"<h2 style='color:red'> Error</h2>"
-
-                                                         },
-                                                         head <- obj$head
-                                         )
-                        } else {
-                                 head<-"<div><i Note:></i></div>"
+private = list(
+                .warnings = list(),
+                .errors = list(),
+                .process_html = function(content, obj) {
+                    
+                    style = ""
+                    title = ""
+                    
+                    # Contenitore dell'icona con flex e padding
+                    icon_container_style = "display: flex; align-items: center; padding: 10px;"
+                    
+                    # Dimensioni fisse per l'icona e colore di default
+                    icon_style = "font-size: 50px; flex-shrink: 0;"
+                    
+                    if (is.something(obj$head)) {
+                        switch (obj$head,
+                                "info" = {
+                                    # SVG per messaggio informativo
+                                    head <- paste0("<div style='", icon_container_style, "'><svg width='50' height='50' xmlns='http://www.w3.org/2000/svg'><circle cx='25' cy='25' r='20' fill='#3e6da9'></circle><text x='50%' y='55%' dominant-baseline='middle' text-anchor='middle' fill='white' font-size='30' font-family='Arial Black'>i</text></svg></div>")
+                                    style <- "border-color: #3e6da9;"
+                                },
+                                "warning" = {
+                                    # SVG per messaggio di avviso
+                                    head <- paste0("<div style='", icon_container_style, "'><svg width='50' height='50' xmlns='http://www.w3.org/2000/svg'><circle cx='25' cy='25' r='20' fill='orange'></circle><text x='50%' y='55%' dominant-baseline='middle' text-anchor='middle' fill='white' font-size='30' font-family='Arial Black'>!</text></svg></div>")
+                                    style <- "border-left-color: orange;"
+                                    title <- "<h2 style='color:orange;'> Warning</h2>"
+                                },
+                                "error" = {
+                                    # SVG per messaggio di errore
+                                    head <- paste0("<div style='", icon_container_style, "'><svg width='50' height='50' xmlns='http://www.w3.org/2000/svg'><circle cx='25' cy='25' r='20' fill='red'></circle><text x='50%' y='55%' dominant-baseline='middle' text-anchor='middle' fill='white' font-size='25' font-family='Arial Black'>X</text></svg></div>")
+                                    style <- "border-color: red;"
+                                    title <- "<h2 style='color:red;'> Error</h2>"
+                                },
+                                {
+                                    head <- obj$head
+                                }
+                        )
+                    } else {
+                        head <- "<div><i>Note:</i></div>"
+                    }
+                    
+                    test <- grep(obj$message, content, fixed = TRUE)
+                    if (length(test) == 0) {
+                        content <- paste0(content, "<div class='notice-box' style='", style, "'>", head, "<div class='content'>", title, obj$message, "</div></div>")
+                    }
+                    
+                    return(content)
+                },
+                
+                .find_table = function(path) {
+                    tableobj <- self$tables
+                    found <- FALSE
+                    for (aname in path) {
+                        if (hasName(tableobj, aname)) {
+                            found <- TRUE
+                            tableobj <- tableobj[[aname]]
                         }
-
-                        
-                        test<-grep(obj$message,content,fixed=TRUE)
-                        if (length(test) == 0)
-                                     content<-paste(content,"<div class='notice-box' style='",style, "'>",head,"<div class='content'>",title,obj$message,"</div></div>")
-                              return(content)
-                       },
-                      .find_table=function(path) {
-
-                        tableobj<-self$tables
-                        found<-FALSE
-                        for (aname in path)
-                          if (hasName(tableobj,aname)) {
-                            found<-TRUE
-                            tableobj<-tableobj[[aname]]
-                          }
-                        if (found)
-                             return(tableobj)
-                        else
-                             return(NULL)
-                        
-                      }
-                       
-            ) #end of private
+                    }
+                    if (found)
+                        return(tableobj)
+                    else
+                        return(NULL)
+                }
+            ) # end of private            
 ) #end of class
 
 
