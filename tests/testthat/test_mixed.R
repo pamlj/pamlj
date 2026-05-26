0af260b941295c084ed188ee8cd3cf10684a6fea
Marcello Gallucci <mcfanda@gmail.com>
2026-05-26 16:45:16 +0200

    ! x

--- tests/testthat/test_mixed.R
+++ tests/testthat/test_mixed.R
@@ -2,10 +2,12 @@ testthat::context("mixed")
 tol<-.0001
 
 m<-"
-y~0*1+.65*x+a*.3*z+(1*1+5.8*x|cluster)
-bet:a|cluster
-test:a
+y~0*1+.65*x+a*.3*z+(1*1+5.8*x|cluster)+(1*1|cluster2)
+bet:x|cluster
+within:z|cluster
+test: a
+expand: cluster2
 "
-pamlj::pamlmixed(aim="n",find="n",syntax = m,clusterpars = list(cluster=c(n=10,k=20)),algo="raw")
+pamlj::pamlmixed(aim="n",find="k",syntax = m,clusterpars = list(cluster=c(n=10,k=20),cluster2=c(n=2,k=10)),algo="raw",sigma2 = 30)
 
 
